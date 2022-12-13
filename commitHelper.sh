fa7a4f8bdc1aec5a7dec657e74089b4aba1b71b1
1034001 <gustavo.santos@novomundo.com.br>
2022-12-13 10:21:48 -0300

    :test_tube: test: testes alias 2

--- commitHelper.sh
+++ commitHelper.sh
@@ -19,7 +19,7 @@ create_alias_zsh() {
 
     alias_value=`cat /home/$current_user/.zshrc | grep "gch="`
 
-    if [[ -n "$alias_value" ]]; then
+    if [[ -z "$alias_value" ]]; then
     echo "Criando alias"
         sed -i "/Example aliases/a\ alias gch=\"$1\"" /home/$current_user/.zshrc && exec `source ./zshrc`
     fi
